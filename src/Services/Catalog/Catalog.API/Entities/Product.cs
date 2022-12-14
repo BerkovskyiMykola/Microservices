6136805112838eaec91d6cb5bf8f16f617ab3995
Mykola Berkovskyi <nomvodafon@gmail.com>
2022-12-14 18:21:53 +0200

    edit id

--- src/Services/Catalog/Catalog.API/Entities/Product.cs
+++ src/Services/Catalog/Catalog.API/Entities/Product.cs
@@ -7,7 +7,7 @@ namespace Catalog.API.Entities
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        public string? Id { get; set; }
+        public string Id { get; set; } = string.Empty;
 
         public string Name { get; set; } = string.Empty;
         public string Category { get; set; } = string.Empty;
