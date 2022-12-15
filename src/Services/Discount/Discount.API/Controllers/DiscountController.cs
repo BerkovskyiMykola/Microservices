20d53dab29f6a5ee01a2182bd90737008f3c5d09
Mykola Berkovskyi <nomvodafon@gmail.com>
2022-12-15 15:44:53 +0200

    add the logic of interaction with the database

---
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,50 @@
+﻿using Discount.API.Entities;
+using Discount.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Xml.Linq;
+
+namespace Discount.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class DiscountController : ControllerBase
+    {
+        private readonly IDiscountRepository _repository;
+
+        public DiscountController(IDiscountRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet("{productName}", Name = "GetDiscount")]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetDiscount(string productName)
+        {
+            var coupon = await _repository.GetDiscountAsync(productName);
+            return Ok(coupon);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> CreateDiscount([FromBody] Coupon coupon)
+        {
+            await _repository.CreateDiscountAsync(coupon);
+            return CreatedAtRoute("GetDiscount", new { productName = coupon.ProductName }, coupon);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> UpdateDiscount([FromBody] Coupon coupon)
+        {
+            return Ok(await _repository.UpdateDiscountAsync(coupon));
+        }
+
+        [HttpDelete("{productName}", Name = "DeleteDiscount")]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteDiscount(string productName)
+        {
+            return Ok(await _repository.DeleteDiscountAsync(productName));
+        }
+    }
+}
--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,4 +1,8 @@
 
+using Discount.API.Extensions;
+using Discount.API.Repositories;
+using Microsoft.OpenApi.Models;
+
 namespace Discount.API
 {
     public class Program
@@ -10,9 +14,13 @@ namespace Discount.API
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Discount.API", Version = "v1" });
+            });
+
+            builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
             var app = builder.Build();
 
@@ -20,13 +28,15 @@ namespace Discount.API
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Discount.API v1"));
             }
 
             app.UseAuthorization();
 
 
             app.MapControllers();
+
+            app.MigrateDatabase<Program>();
 
             app.Run();
         }
